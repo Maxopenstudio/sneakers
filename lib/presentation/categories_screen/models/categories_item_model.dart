2688aa750f8f77d7ea54ec26497ba4bea838ae98
Oubi256 <98897017+Oubi256@users.noreply.github.com>
2023-12-05 17:22:10 +0100

    bckp

--- lib/presentation/categories_screen/models/categories_item_model.dart
+++ lib/presentation/categories_screen/models/categories_item_model.dart
@@ -1,6 +1,29 @@
 class CategoriesItemModel {
+  int categoryId;
+  int parentId;
   String image;
   String name;
+  List<CategoriesItemModel> categories;
 
-  CategoriesItemModel(this.image, this.name);
+  CategoriesItemModel({required this.image, required this.name, required this.categoryId, required this.parentId, required this.categories});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'categoryId': this.categoryId,
+      'parentId': this.parentId,
+      'image': this.image,
+      'name': this.name,
+      'categories': this.categories,
+    };
+  }
+
+  factory CategoriesItemModel.fromMap(Map<String, dynamic> map) {
+    return CategoriesItemModel(
+      categoryId: map['categoryId'] as int,
+      parentId: map['parentId'] as int,
+      image: map['image'] as String,
+      name: map['name'] as String,
+      categories: map['categories'] as List<CategoriesItemModel>,
+    );
+  }
 }
--- lib/presentation/categories_screen/models/categories_model.dart
+++ lib/presentation/categories_screen/models/categories_model.dart
@@ -4,12 +4,12 @@ import 'categories_item_model.dart';
 
 class CategoriesModel {
   RxList<CategoriesItemModel> categoryItemList = [
-    CategoriesItemModel("cat1.png", "Running"),
-    CategoriesItemModel("cat2.png", "Sneakers"),
-    CategoriesItemModel("cat3.png", "Originals"),
-    CategoriesItemModel("cat4.png", "Walking"),
-    CategoriesItemModel("cat5.png", "Slids & Sandles"),
-    CategoriesItemModel("cat6.png", "Foot Ball"),
-    CategoriesItemModel("cat7.png", "Basket Ball"),
+    CategoriesItemModel("cat1.png", "Running", 0, 0, []),
+    CategoriesItemModel("cat2.png", "Sneakers", 0, 0, []),
+    CategoriesItemModel("cat3.png", "Originals", 0, 0, []),
+    CategoriesItemModel("cat4.png", "Walking", 0, 0, []),
+    CategoriesItemModel("cat5.png", "Slids & Sandles", 0, 0, []),
+    CategoriesItemModel("cat6.png", "Foot Ball", 0, 0, []),
+    CategoriesItemModel("cat7.png", "Basket Ball", 0, 0, []),
   ].obs;
 }
